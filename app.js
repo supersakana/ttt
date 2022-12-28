24b612c21d5e5c6ef43888ca144e12966115ae7e
supersakana <zacwilliamson@icloud.com>
2022-12-28 17:28:38 -0600

    created basic layout and appended cells to the dom

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('test')
+const gameBoard = (() => {
+    let board = document.querySelector('#board')
+    let cells = []
+    
+    const create = () => {
+        for(i = 0; i < 9; i++){
+            let cell = document.createElement('div')
+            cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
+            cell.dataset.mark = ''
+            cells.push(cell)
+            board.append(cell)
+        }
+    }
+    return { create };
+})();
+
+gameBoard.create()
