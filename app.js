890ec226acf1a23783780241521bfd30c2b8c03b
supersakana <zacwilliamson@icloud.com>
2022-12-29 11:58:58 -0600

    player can not mark cell that is already marked

--- app.js
+++ app.js
@@ -16,19 +16,17 @@ const game = (() => {
         players.push(p2)
     }
 
-    // swaps player and returns current player's marker
-    const makeMove = () => {
+    // swaps player and returns current player's symbol
+    const makeMark = () => {
         swap()
-        return players[1].marker
+        return players[1].sym
     }
 
     // swaps player position in array
     const swap = () => {
-        current = players[0]
-        players[0] = players[1]
-        players[1] = current
+        [players[0], players[1]] = [players[1], players[0]]
     }
-    return { start, makeMove };
+    return { start, makeMark };
 })();
 
 // module pattern
@@ -40,7 +38,13 @@ const board = (() => {
         for(i = 0; i < 9; i++){
             let cell = cellFactory(i);
             cells.push(i);
-            cell.addEventListener('click', () => { cell.append(game.makeMove()) })
+            cell.addEventListener('click', () => { 
+                if(cell.dataset.sym == ''){
+                    mark = game.makeMark()
+                    cell.dataset.sym = mark
+                    cell.append(mark)
+                }
+            })
         }
     }
     return { create };
@@ -51,15 +55,15 @@ const cellFactory = (n) => {
     // returns and append new cell
     let cell = document.createElement('div')
     cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
-    // cell.dataset.mark = ''
+    cell.dataset.sym = ''
     document.getElementById('board').append(cell)
-    return  cell
+    return cell
 };
 
 // factory function
-const playerFactory = (m) => {
-    let marker = m
-    return { marker };
+const playerFactory = (s) => {
+    let sym = s
+    return { sym };
 };
 
 game.start()
