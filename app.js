5f67e788a3fc3e6a0a127d5574dd6cffdecb2751
supersakana <zacwilliamson@icloud.com>
2022-12-29 10:07:25 -0600

    created game module pattern that will hold game flow logic along with player factory function

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 // module pattern
-const gameBoard = (() => {
+const game = (() => {   
+    const players = []
+
+    // initializes a new game
+    const start = () => {
+        board.create()
+        twoPlayers()
+    }
+
+    // creates 2 players
+    const twoPlayers = () => {
+        let p1 = playerFactory('X')
+        players.push(p1)
+        let p2 = playerFactory('O')
+        players.push(p2)
+    }
+    return { start };
+})();
+
+// module pattern
+const board = (() => {
     let board = document.querySelector('#board')
     let cells = []
     
+    // creates and appends board cells
     const create = () => {
         for(i = 0; i < 9; i++){
             let cell = cellFactory();
-            cells.push(cell.element(i));
+            cells.push(i);
             board.append(cell.element(i));
         }
     }
@@ -15,6 +36,7 @@ const gameBoard = (() => {
 
 // factory function
 const cellFactory = () => {
+    // returns cell element to be appended
     const element = (n) => {
         let newCell = document.createElement('div')
         newCell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
@@ -26,4 +48,11 @@ const cellFactory = () => {
     return { element };
 };
 
-gameBoard.create()
+// factory function
+const playerFactory = (m) => {
+    let marker = m
+    return { marker };
+};
+
+
+game.start()
