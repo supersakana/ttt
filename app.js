2d2c38d8642152717a2e541d891ac7b0d5cbccae
supersakana <zacwilliamson@icloud.com>
2022-12-29 09:07:08 -0600

    created cell factory function

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+// module pattern
 const gameBoard = (() => {
     let board = document.querySelector('#board')
     let cells = []
     
     const create = () => {
         for(i = 0; i < 9; i++){
-            let cell = document.createElement('div')
-            cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
-            cell.dataset.mark = ''
-            cells.push(cell)
-            board.append(cell)
+            let cell = cellFactory();
+            cells.push(cell.element(i));
+            board.append(cell.element(i));
         }
     }
     return { create };
 })();
+
+// factory function
+const cellFactory = () => {
+    const element = (n) => {
+        let newCell = document.createElement('div')
+        newCell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
+        newCell.id = `cell-${n}`
+        newCell.dataset.mark = ''
+
+        return  newCell
+    }
+    return { element };
+};
 
 gameBoard.create()
