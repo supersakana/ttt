734e29dadfa382470917ca4861bf4e2e72c112c7
supersakana <zacwilliamson@icloud.com>
2022-12-30 13:55:46 -0600

    created solution that determines win

--- app.js
+++ app.js
@@ -26,12 +26,21 @@ const game = (() => {
     const swap = () => {
         [players[0], players[1]] = [players[1], players[0]]
     }
-    return { start, makeMark };
+
+    const end = () => {
+        if(board.winExsists(players[1].sym)){
+            console.log('woo')
+        }
+    }
+    return { start, makeMark, end };
 })();
 
 // module pattern
 const board = (() => {
     let cells = []
+    const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                  [0, 4, 8], [6, 4, 2]]
     
     // creates and appends board cells
     const create = () => {
@@ -43,19 +52,27 @@ const board = (() => {
                     mark = game.makeMark()
                     cell.dataset.sym = mark
                     cell.append(mark)
+                    cells[parseInt(cell.dataset.no)] = mark
+                    game.end()
                 }
             })
         }
     }
-    return { create };
+
+    // returns true if there is a winning combo on the board
+    const winExsists = (sym) => {
+        return wins.some(combo => combo.every(cell => cells[cell] == sym))
+    }
+    return { cells, create, winExsists };
 })();
 
 // factory function
 const cellFactory = (n) => {
     // returns and append new cell
     let cell = document.createElement('div')
-    cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer')
+    cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer', 'flex', 'justify-center', 'items-center')
     cell.dataset.sym = ''
+    cell.dataset.no = n
     document.getElementById('board').append(cell)
     return cell
 };
