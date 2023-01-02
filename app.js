dd25e83ef91dd94144138d808308ce8648cf2c98
supersakana <zacwilliamson@icloud.com>
2023-01-02 08:24:15 -0600

    Winner message is now displayed when win detected

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
 // module pattern
-const game = (() => {   
+const Game = (() => {   
     const players = []
 
-    // initializes a new game
     const start = () => {
-        board.create()
+         Board.create()
         twoPlayers()
     }
 
-    // creates 2 players
     const twoPlayers = () => {
-        let p1 = playerFactory('X')
-        let p2 = playerFactory('O')
+        let p1 = playerFactory('X', 1)
+        let p2 = playerFactory('O', 2)
         players.push(p1, p2)
     }
 
-    // swaps player and returns current player's symbol
     const currentPlayer = () => {
         swap()
-        return players[1].sym
+        return players[1]
     }
 
-    // swaps player position in array
     const swap = () => {
         [players[0], players[1]] = [players[1], players[0]]
     }
 
-    // ends the game if tie or winner
-    const end = () => {
-        if(board.winExsists(players[1].sym)){
-            console.log('woo')
+    const end = (player) => {
+        if(Board.winExsists(player.sym)){
+            Display.winner(player)
         }
     }
     return { start, currentPlayer, end };
 })();
 
-// module pattern
-const board = (() => {
+const Display = (() => {
+    const winner = (player) => {
+        let element = document.querySelector('.winner')
+        element.innerHTML = `${player.name} is the winner!`
+    }
+    return { winner }
+})();
+
+const Board = (() => {
     let cells = []
     const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                   [0, 3, 6], [1, 4, 7], [2, 5, 8],
                   [0, 4, 8], [6, 4, 2]]
     
-    // creates and appends board cells
     const create = () => {
         for(i = 0; i < 9; i++){
             let cell = cellFactory(i);
@@ -53,27 +54,23 @@ const board = (() => {
         }
     }
 
-    // returns true if there is a winning combo on the board
     const markBoard = (cell) => {
         if(cell.dataset.sym == ''){
-            mark = game.currentPlayer()
-            cell.dataset.sym = mark
-            cell.append(mark)
-            cells[parseInt(cell.dataset.no)] = mark
-            game.end()
+            current = Game.currentPlayer()
+            cell.dataset.sym = current.sym
+            cell.append(current.sym)
+            cells[parseInt(cell.dataset.no)] = current.sym
+            Game.end(current)
         }
     }
 
-    // returns true if there is a winning combo on the board
     const winExsists = (sym) => {
         return wins.some(combo => combo.every(cell => cells[cell] == sym))
     }
     return { cells, create, winExsists };
 })();
 
-// factory function
 const cellFactory = (n) => {
-    // returns and append new cell
     let cell = document.createElement('div')
     cell.classList.add('w-20', 'h-20', 'bg-white', 'rounded-md', 'shadow-md', 'hover:cursor-pointer', 'flex', 'justify-center', 'items-center')
     cell.dataset.sym = ''
@@ -82,9 +79,11 @@ const cellFactory = (n) => {
     return cell
 };
 
-// factory function
-const playerFactory = (sym) => {
-    return { sym };
+const playerFactory = (sym, no) => {
+    let name = `Player ${no}`
+    return { sym, name };
 };
 
-game.start()
+
+
+Game.start()
